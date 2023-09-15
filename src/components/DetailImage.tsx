0d317cd1e9b8e2a29157dfd47e59ba09feafcb6d
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-09-15 16:35:38 +0700

    Add zoom in image of detail product

--- src/components/DetailImage.tsx
+++ src/components/DetailImage.tsx
@@ -8,6 +8,22 @@ interface DetailImageProps {
 const DetailImage: React.FC<DetailImageProps> = ({ thumbnail, images }) => {
   const [currentImg, setCurrentImg] = useState<string>('');
   const [isThumbnail, setIsThumbnail] = useState<boolean>(true);
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  // eslint-disable-next-line no-unused-vars
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsZoomed(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsZoomed(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
   const imageRef = useRef<HTMLImageElement | null>(null);
   const handleChangeProductImage = (imgSource: string, _isThumbnail: boolean) => {
     if (imageRef && imageRef.current) {
@@ -22,8 +38,24 @@ const DetailImage: React.FC<DetailImageProps> = ({ thumbnail, images }) => {
   };
   return (
     <div>
-        <div className='rounded overflow-hidden'>
-            <img ref={imageRef} src={thumbnail} className='w-full object-cover h-[25rem] transition-all ease duration-300' alt="" />
+        <div
+            className='rounded overflow-hidden w-full h-[25rem] relative transition-all ease-in-out duration-300 cursor-zoom-in'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onMouseMove={handleMouseMove}
+        >
+            <img
+                ref={imageRef}
+                src={thumbnail}
+                className={`w-full object-cover h-full  ${
+                    isZoomed ? 'relative scale-150' : 'relative scale-100'
+                }`}
+                alt=""
+                style={{
+                    top: isZoomed ? `-${cursorPosition.y / 7}px` : 0,
+                    left: isZoomed ? `-${cursorPosition.x / 7}px` : 0,
+                }}
+            />
         </div>
         <div className="mt-3 flex flex-wrap justify-center items-center gap-2">
             <div
