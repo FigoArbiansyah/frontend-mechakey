c1054133ae98116f14d7bef34028f8aec5469743
FigoArbiansyah <figoarbnsyh@gmail.com>
2023-09-16 15:20:10 +0700

    Add button for add to cart and buy now

--- src/pages/DetailProduct.tsx
+++ src/pages/DetailProduct.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import products from '../dummies/products';
 import GradientBadge from '../components/GradientBadge';
@@ -8,7 +8,19 @@ import DetailImage from '../components/DetailImage';
 
 const DetailProduct: React.FC = () => {
   const { id } = useParams();
+  const [count, setCount] = useState<number>(1);
   const filteredProducts = products.filter((product) => Number(product?.id) === Number(id))[0];
+  const counterForCheckout = (_isAdding: boolean) => {
+    if (_isAdding) {
+        if (count < filteredProducts?.stock) {
+            setCount(count + 1);
+        }
+    } else {
+        if (count !== 1) {
+            setCount(count - 1);
+        }
+    }
+  };
   
   return (
     <section className='min-h-screen md:p-24 p-5'>
@@ -54,6 +66,40 @@ const DetailProduct: React.FC = () => {
                         <TableRow rowTitle='Backlight' rowCell={filteredProducts?.backlight ? 'Yes' : 'No'} />
                         <TableRow rowTitle='Layout' rowCell={filteredProducts?.layout} />
                         <TableRow rowTitle='Switch' rowCell={filteredProducts?.switch_type} />
+                    </div>
+                    <div className="mt-10">
+                        <div className="flex justify-between items-center gap-5">
+                            <p className="text-slate-900" style={{
+                                flex: 3,
+                            }}>Jumlah : </p>
+                            <div style={{
+                                flex: 1,
+                            }} className='flex justify-between items-center'>
+                                <button
+                                onClick={() => counterForCheckout(false)}
+                                className='border p-1.5 rounded'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                                    </svg>
+                                </button>
+                                <span>{count}</span>
+                                <button
+                                onClick={() => counterForCheckout(true)}
+                                className='border p-1.5 rounded'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="mt-6 grid grid-cols-2 gap-5">
+                            <button
+                              className='p-2 border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white transition-all ease'
+                            >Add to Cart</button>
+                            <button
+                              className='p-2 hover:bg-red-400 bg-rose-500 text-white transition-all ease'
+                            >Buy Now</button>
+                        </div>
                     </div>
                 </div>
             </div>
